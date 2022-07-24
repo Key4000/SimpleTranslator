11832c3f0ad8c4cf0f16282bd4dae11be74d5f1f
Key4000 <pvlzhizhin@mail.ru>
2022-07-24 17:29:29 +0500

    Добавьте файлы проекта.

---
+++ SimpleTranslator/SimpleTranslator.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <ctype.h>
+#include <cstdio>
+
+using namespace std;
+
+class Parser {
+
+    static int lookahead;
+
+public:
+    Parser() {
+        cin >> lookahead;
+    }
+    void expr() {
+        term();
+        while (true) {
+            if (lookahead == '+') {
+                match('+'); term(); std::cout << '+';
+            }
+            else if (lookahead == '-')
+            {
+                match('-'); term(); std::cout << '-';
+            }
+            else return;
+        }
+    };
+    void term() {
+        if (isdigit((char)lookahead)) {
+            std::cout << (char)lookahead;
+            match(lookahead);
+        }
+    
+    };
+    void match(int t) {
+        if (lookahead == t)
+            std::cin >> lookahead;
+        else 
+            throw("Syntax error");
+    }
+};
+
+class Postfix {
+    public:
+        static void main() {
+            Parser parse = Parser();
+            parse.expr(); std::cout << '\n';
+        }
+
+};
+
+int main()
+{
+    Postfix postfix;
+    postfix.main();
+}
